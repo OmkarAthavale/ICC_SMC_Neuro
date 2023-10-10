ad9f82b88bf56acf8b4be1c830f16fbfa7d3ae13
Omkar Athavale <oath399@UoA.auckland.ac.nz>
2023-10-10 15:52:28 +1300

    Added keIP3 to the variance absed sensitivity analysis

--- sensitivity_analysis/SA2_analysis_plot.py
+++ sensitivity_analysis/SA2_analysis_plot.py
@@ -7,12 +7,13 @@ from matplotlib import pyplot as plt
 
 print("Def SA problem")
 problemDef = {
-    'num_vars': 4,
-    'names': ['k_iAno1', 'k_iNSCC', 'k_iCa50', 'k_iSK'],
+    'num_vars': 5,
+    'names': ['k_iAno1', 'k_iNSCC', 'k_iCa50', 'k_iSK', 'k_eIP3'],
     'bounds': [[0.25355, 0.35258],
                [0.76517, 0.79896],
                [0.38393, 0.40164],
-               [0.29620, 0.33465]
+               [0.29620, 0.33465],
+               [0.82, 1]
               ]
 }
 
@@ -25,23 +26,23 @@ Si_amp = sobol.analyze(problemDef, Y_amp)
 
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(4.48,4), tight_layout=True)
 
-axes[0, 0].bar(np.array(np.linspace(0, 3, 4)), Si_amp['S1'], tick_label=problemDef['names'], yerr=Si_amp['S1_conf'], fill=False)
+axes[0, 0].bar(np.array(np.linspace(0, problemDef['num_vars']-1, problemDef['num_vars'])), Si_amp['S1'], tick_label=problemDef['names'], yerr=Si_amp['S1_conf'], fill=False)
 axes[0, 0].set_ylim(0, 1.2)
 axes[0, 0].set_xticklabels([])
 axes[0, 0].set_ylabel('$\mathregular{S_1}$')
 axes[0, 0].set_title('Amplitude Metric')
 
-axes[0, 1].bar(np.array(np.linspace(0, 3, 4)), Si_freq['S1'], tick_label=problemDef['names'], yerr=Si_freq['S1_conf'], fill=False)
+axes[0, 1].bar(np.array(np.linspace(0, problemDef['num_vars']-1, problemDef['num_vars'])), Si_freq['S1'], tick_label=problemDef['names'], yerr=Si_freq['S1_conf'], fill=False)
 axes[0, 1].set_ylim(0, 1.2)
 axes[0, 1].set_xticklabels([])
 axes[0, 1].set_yticklabels([])
 axes[0, 1].set_title('Frequency Metric')
 
-axes[1, 0].bar(np.array(np.linspace(0, 3, 4)), Si_amp['ST'], tick_label=problemDef['names'], yerr=Si_amp['ST_conf'], fill=False)
+axes[1, 0].bar(np.array(np.linspace(0, problemDef['num_vars']-1, problemDef['num_vars'])), Si_amp['ST'], tick_label=problemDef['names'], yerr=Si_amp['ST_conf'], fill=False)
 axes[1, 0].set_ylim(0, 1.2)
 axes[1, 0].set_ylabel('$\mathregular{S_T}$')
 
-axes[1, 1].bar(np.array(np.linspace(0, 3, 4)), Si_freq['ST'], tick_label=problemDef['names'], yerr=Si_freq['ST_conf'], fill=False)
+axes[1, 1].bar(np.array(np.linspace(0, problemDef['num_vars']-1, problemDef['num_vars'])), Si_freq['ST'], tick_label=problemDef['names'], yerr=Si_freq['ST_conf'], fill=False)
 axes[1, 1].set_ylim(0, 1.2)
 axes[1, 1].set_yticklabels([])
 
